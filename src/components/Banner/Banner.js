68515d69749a269a8e3bbaa269fe76a9715e18f6
Rohit Parihar <rohitparihar07125@gmail.com>
2022-07-12 09:51:25 +0530

    Corrected spelling mistake

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -41,7 +41,7 @@ function Banner() {
               fontFamily: "Montserrat",
             }}
           >
-            Crypto Hunter
+            Crypro Bazzar
           </Typography>
           <Typography
             variant="subtitle2"
