b0e9188ba2d3bf3b0992e5321ae5845ba0daec11
Rohit Parihar <rohitparihar07125@gmail.com>
2022-07-12 11:45:16 +0530

    added firebase

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -41,7 +41,7 @@ function Banner() {
               fontFamily: "Montserrat",
             }}
           >
-            Crypto Bazzar
+            Crypto Hunter
           </Typography>
           <Typography
             variant="subtitle2"
---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDa0G2jC-BWzYMwlNi6LMl2_F38f0cUUQc",
+  authDomain: "crypto-bazzar-ea8d8.firebaseapp.com",
+  projectId: "crypto-bazzar-ea8d8",
+  storageBucket: "crypto-bazzar-ea8d8.appspot.com",
+  messagingSenderId: "68910207227",
+  appId: "1:68910207227:web:1f24d2d076d057fa9da05e",
+  measurementId: "G-RM3LFMMVX0"
+};
+export default firebaseConfig;
